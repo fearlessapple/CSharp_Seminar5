5ce2cfacba91ba6185f7f2c42df36912f62ef4ad
Stas <smaster950@gmail.com>
2022-10-20 23:04:36 +0300

    Complete Exercise34

--- Exercise34/Program.cs
+++ Exercise34/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Задайте размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "");
+
+int[] array = GetArray(size, 100, 999);
+Console.WriteLine(String.Join(" ", array));
+ArrayCheck(array);
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void ArrayCheck(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum = sum + 1;
+        }
+        else
+        {
+            sum = sum + 0;
+        }  
+    }
+    Console.Write($"Чётных чиселв массиве: {sum}");
+}
