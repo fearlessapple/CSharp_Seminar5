c01b92388a47a18cd9e0bb722bf508f61d7d0237
Stas <smaster950@gmail.com>
2022-10-21 18:44:03 +0300

    Complete Exercise36

--- Exercise36/Program.cs
+++ Exercise36/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Задайте размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Задайте минимальное значение эл-та массива: ");
+int minValue = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Задайте максимальное значение эл-та массива: ");
+int maxValue = int.Parse(Console.ReadLine() ?? "");
+
+int[] array = GetArray(size, minValue, maxValue);
+Console.WriteLine(String.Join(" ", array));
+SumElements(array);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void SumElements(int[] array)
+{
+    double sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+        else
+        {
+            sum = sum + 0;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма нечётных эл-тов массива: {sum + array[0]}");
+}
