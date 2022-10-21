6b826df8deedbe3e3ec63f2323758b2f6f68ecbf
Stas <smaster950@gmail.com>
2022-10-21 19:42:08 +0300

    Complete Exercise38

--- Exercise38/Program.cs
+++ Exercise38/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Задайте размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Задайте минимальное значение эл-та массива: ");
+int minValue = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Задайте максимальное значение эл-та массива: ");
+int maxValue = int.Parse(Console.ReadLine() ?? "");
+
+int[] array = GetArray(size, minValue, maxValue);
+Console.WriteLine(String.Join(" ", array));
+
+MaxElement(array);
+MinElement(array);
+
+int Diff = MaxElement(array) - MinElement(array);
+Console.WriteLine($"Разница между максимальным и минимальным: {Diff}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int MaxElement(int[] array)
+{
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+int MinElement(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+    }
+    return min;
+}
